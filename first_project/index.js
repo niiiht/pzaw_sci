41635dfe8f9a86abdb03e58b3b9f50ce45f927f6
Natalia Burzyńska <natkamb@o2.pl>
2025-10-06 06:37:12 +0000

    projekt 1

---
+++ first_project/index.js
@@ -0,0 +1,38 @@
+import { createServer } from 'node:http';
+import {URL} from "node:url"
+import { readFileSync } from "node:fs";
+
+const inhtml = readFileSync("public/index.html")
+
+const server = createServer((req, res) => {
+
+  const url = new URL(`http://${host}${req.url}`);
+  const pa = url.pathname;
+  console.log(`Request: ${req.method} ${pa}`);
+
+
+  if(pa === "/" && req.method=== "GET"){
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(inhtml);
+  }
+  if (!res.writableEnded) {
+
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+
+    res.end('Site not found!\n');
+
+  }
+});
+  const port = 8000;
+
+const host = "localhost";
+
+
+// Start the server
+
+server.listen(port, host, () => {
+
+    console.log(`Server listening on http://${host}:${port}`);
+  
+});
+
