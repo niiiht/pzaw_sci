bd86363cc918426528fd44941291386eeced3032
Natalia Burzyńska <natkamb@o2.pl>
2025-10-16 08:36:25 +0000

    changes 3

--- first_project/index.js
+++ first_project/index.js
@@ -3,7 +3,7 @@ import {URL} from "node:url"
 import { readFileSync } from "node:fs";
 
 const inhtml = readFileSync("public/index.html")
-
+const icon = readFileSync("public/favicon.ico")
 const server = createServer((req, res) => {
 
   const url = new URL(`http://${host}${req.url}`);
@@ -15,6 +15,14 @@ const server = createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(inhtml);
   }
+
+  if(req.url === '/favicon.ico' && req.method=== "GET"){
+
+  res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+
+    res.end(icon);
+  }
+
   if (!res.writableEnded) {
 
     res.writeHead(404, { 'Content-Type': 'text/plain' });
