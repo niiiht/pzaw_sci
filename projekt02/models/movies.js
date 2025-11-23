53915e495ca0057961f81dc5fa858e7eeb5df621
Natalia Burzyńska <natkamb@o2.pl>
2025-11-23 22:24:52 +0000

    final

--- projekt02/models/movies.js
+++ projekt02/models/movies.js
@@ -1,6 +1,7 @@
 const movie_list = [{name:"Matrix"}, { name: "Bliskie spotkanie trzeciego stopnia"}, {name:"Goonies"}];
 export function addToList(movie) {
-movie_list.push(movie);
+movie_list.push({name:movie});
+
 } 
 export function mapout(){
 return movie_list.map(movie => ({ name: movie.name }));
