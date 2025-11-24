5beb510090afa9cf11f3e710941425143b97a8af
Natalia Burzyńska <natkamb@o2.pl>
2025-11-24 07:26:31 +0000

    final2

--- projekt02/README.md
+++
@@ -1,3 +0,0 @@
-uruchomienie programu:
-wpisać w terminalu vscode  npm install express
-następnie należy wpisać node index.js
--- projekt02/index.js
+++ projekt02/index.js
@@ -7,19 +7,28 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set('views', './views');
+app.use(express.urlencoded());
 
-app.get("/", (req, res) => { 
+app.get("/", (req,res) => {
+  res.redirect(`/filmy`);
+  console.log("GET / działa poprawnie");
+})
+
+app.get("/filmy", (req, res) => { 
 res.render("filmy",{
     title:"filmy",
     nazwa: movies.mapout(),
 
 });
-console.log("GET / działa poprawnie");
+console.log("GET /filmy działa poprawnie");
 });
 
-app.post("/", (req,res)=>{
-  console.log("dupa");  
-})
+app.post("/filmy/nowe", (req,res)=>{
+movies.addToList(req.body.movie_name);
+
+  res.redirect(`/filmy`);
+  console.log("post działa poprawnie"); 
+   });
 app.listen(port,() =>{
     console.log(`Server listening on http://localhost:${port}`);
 });
